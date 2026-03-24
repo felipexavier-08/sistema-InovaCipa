87afbf4c8881cc11cbda99747c7918f548314009
Felipe Xavier Reis de Jesus <166186256+felipexavier-08@users.noreply.github.com>
2026-03-24 10:59:43 -0300

    Update email_brevo.php

--- code/cipa_t1/config/email_brevo.php
+++ code/cipa_t1/config/email_brevo.php
@@ -2,7 +2,7 @@
 // Configurações de Email - Brevo API
 return [
     'api_key' => '', // SUBSTITUIR COM A CHAVE API
-    'from_email' => 'fxdev08@gmail.com',
+    'from_email' => ''//email brevo,
     'from_name' => 'Sistema CIPA',
     'api_url' => 'https://api.brevo.com/v3/smtp/email'
 ];
